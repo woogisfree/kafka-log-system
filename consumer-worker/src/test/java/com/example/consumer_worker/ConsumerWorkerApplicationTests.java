1ac7eaad6c1b63a84642828ae39e687348142a3e
woogie <woogisfree@gmail.com>
2025-04-15 15:59:39 +0900

    chore: initialize Spring Boot project from start.spring.io

---
+++ consumer-worker/src/test/java/com/example/consumer_worker/ConsumerWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumer_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
